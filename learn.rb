ff556efcc6f1d91bc62b18015bcf13a96268cada
atippax <atip.nasakun.work@gmail.com>
2026-03-26 06:58:39 +0700

    add filter

--- learn.rb
+++ learn.rb
@@ -8,13 +8,17 @@ require 'set'
 # @return [Integer]
 def find_anwser_in_list(target,list,seen)
   return [] if list.empty?
-    current = list[0]
-    remain = target-current
+  current = list[0]
+  remain = target-current
   return [remain,current] if seen.include?(remain)
-    seen.add(current)
-    find_anwser_in_list(target,list[1..-1],seen)
+  seen.add(current)
+  find_anwser_in_list(target,list[1..-1],seen)
 end
 
+def filter_more_than_target(target,list)
+  list.select {|num|num<=target}
+end
 
 input =File.read('input.txt')
-puts find_anwser_in_list(2020,input.split.map{|n|n.to_i},Set.new).reduce(1) {|state,num|num*state}
+target = 2020
+puts find_anwser_in_list(target,filter_more_than_target(target,input.split.map{|n|n.to_i}),Set.new).reduce(1) {|state,num|num*state}
--- test.rb
+++ test.rb
@@ -3,8 +3,13 @@ require "minitest/autorun"
 require_relative "learn"
 
 class  TestA< Minitest::Test
-  def test_find_anwser_in_list
+  def test_find_anwser_in_list_seen
     actrul = find_anwser_in_list(20,[6,2,1,3,4,12,9],Set.new([6,2,8]))
+    assert_equal actrul,[8,12]
+  end
+
+  def test_find_anwser_in_list_never_seen
+    actrul = find_anwser_in_list(20,[1,2,4,5,7,8,123,6,2,1,3,4,12,9],Set.new)
     assert_equal actrul,[8,12]
   end
 end
